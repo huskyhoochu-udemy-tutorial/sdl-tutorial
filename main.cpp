c2296fad79b8ccff182a3e9f21b0eee2c8307ce8
huskyhoochu <dfg1499@gmail.com>
2018-01-25 20:17:34 +0900

    SDL library import

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <SDL.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL init failed." << std::endl;
+        return 1;
+    }
+
+    std::cout << "SDL init succeeded." << std::endl;
+
     return 0;
 }
