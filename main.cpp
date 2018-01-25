3c192186ae60fc53b8ae068eb9a3d77997d07ae1
huskyhoochu <dfg1499@gmail.com>
2018-01-25 20:41:42 +0900

    Set GUI environment

--- main.cpp
+++ main.cpp
@@ -3,12 +3,39 @@
 
 int main() {
 
-    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL init failed." << std::endl;
         return 1;
     }
 
-    std::cout << "SDL init succeeded." << std::endl;
+    SDL_Window *window = SDL_CreateWindow(
+            "Particle Fire Explosion",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (window == NULL) {
+        SDL_Quit();
+        return 2;
+    }
+
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        // Update particles
+        // Draw particles
+        // Check for messages/events
+
+        while (SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
